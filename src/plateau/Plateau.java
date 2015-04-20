a853423fa9b7b2ac9a19f48c356e228275d8f103
Boin Clément <boin.clement@outlook.com>
2015-04-20 20:11:41 +0200

    creation of the plateau initialization method

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -11,5 +11,23 @@ public class Plateau {
     private int length;
     private int width;
 
-    
+    public Plateau(int length, int width, double percentageObstacles) {
+        this.length = length;
+        this.width = width;
+        this.plateau = new Cell[ width ][ length ];
+
+        initializePlateau (this.plateau);
+
+
+    }
+
+    private Cell[][] initializePlateau(Cell[][] tab) {
+        for ( int i = 0; i < tab.length; i++ ) {
+            for ( int j = 0; j < tab[0].length; j++ ) {
+                tab[ i ][ j ] = new Cell (i, j);
+            }
+        }
+
+        return tab;
+    }
 }
