3dcae783f17925fddc080f1d6029bdc88163efd9
Boin Clément <boin.clement@outlook.com>
2015-04-20 20:29:13 +0200

    creation of the obstacles initialization method

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -1,5 +1,7 @@
 package plateau;
 
+import config.Constants;
+
 /**
  * @author boinc
  */
@@ -16,14 +18,37 @@ public class Plateau {
         this.width = width;
         this.plateau = new Cell[ width ][ length ];
 
-        initializePlateau (this.plateau);
+        this.plateau = initializePlateau (this.plateau);
+        initializeObstacles (percentageObstacles);
 
 
     }
 
+    private void initializeObstacles(double percentageObstacles) {
+        int obstacles = 0, x, y;
+
+        while ( obstacles != ( int ) ( ( this.width * this.length ) * percentageObstacles ) ) {
+            x = Constants.random.nextInt (this.width);
+            y = Constants.random.nextInt (this.length);
+
+            if ( this.plateau[ x ][ y ].getBase () == 0 && !this.plateau[ x ][ y ].isObstacle () ) {
+                this.plateau[ x ][ y ].setObstacle (true);
+                obstacles += 1;
+            }
+
+            if ( x == 0 && this.plateau[ x ][ y ].isObstacle () ) {
+                this.plateau[ x ][ y ].setObstacle (false);
+                obstacles -= 1;
+            } else if ( y == this.plateau.length - 1 && this.plateau[ x ][ y ].isObstacle () ) {
+                this.plateau[ x ][ y ].setObstacle (false);
+                obstacles -= 1;
+            }
+        }
+    }
+
     private Cell[][] initializePlateau(Cell[][] tab) {
         for ( int i = 0; i < tab.length; i++ ) {
-            for ( int j = 0; j < tab[0].length; j++ ) {
+            for ( int j = 0; j < tab[ 0 ].length; j++ ) {
                 tab[ i ][ j ] = new Cell (i, j);
             }
         }
