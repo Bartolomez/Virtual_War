18051255ef825f4d8f8b34028317e6048ed4e5ff
Boin Clément <boin.clement@outlook.com>
2015-04-20 19:51:45 +0200

    declaration and assignment of attributes (scanner, random)

---
+++ src/config/Constantes.java
@@ -0,0 +1,14 @@
+package config;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author boinc
+ */
+
+public class Constantes {
+
+    public static final Random random = new Random (0);
+    public static Scanner sc = new Scanner (System.in);
+}
