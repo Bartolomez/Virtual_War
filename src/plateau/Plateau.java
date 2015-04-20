fa5ca376f5599243bb827e4975518fd5c281ac8b
Boin Clément <boin.clement@outlook.com>
2015-04-20 21:02:44 +0200

    implementation of some methods to do action on selected cell and implement the toString methods

--- src/plateau/Plateau.java
+++ src/plateau/Plateau.java
@@ -22,7 +22,7 @@ public class Plateau {
     }
 
     public Cell getCell(int x, int y) {
-        return this.plateau[x][y];
+        return this.plateau[ x ][ y ];
     }
 
     public int getWidth() {
@@ -34,19 +34,40 @@ public class Plateau {
     }
 
     public boolean isObstacle(int x, int y) {
-        return this.plateau[x][y].isObstacle ();
+        return this.plateau[ x ][ y ].isObstacle ();
     }
 
     public int isBase(int x, int y) {
-        return this.plateau[x][y].getBase ();
+        return this.plateau[ x ][ y ].getBase ();
     }
 
     public int isMine(int x, int y) {
-        return this.plateau[x][y].isMine();
+        return this.plateau[ x ][ y ].isMine ();
     }
 
     public int isRobot(int x, int y) {
-        return this.plateau[x][y].isRobot();
+        return this.plateau[ x ][ y ].isRobot ();
+    }
+
+    public void clearCell(int x, int y) {
+        this.plateau[ x ][ y ].clearCell ();
+    }
+
+    public void putRobot(int x, int y, Robot robot) {
+        if ( !this.plateau[ x ][ y ].isObstacle () ) {
+            this.plateau[ x ][ y ].putRobot (robot);
+        } else {
+            System.err.println (
+                "Erreur : La cellule ou vous voulez placer ce robot est un " + "obstacle ! :-(");
+        }
+    }
+
+    public void revokeRobot(Axis axis) {
+        this.plateau[ axis.getHeight () ][ axis.getWidth () ].revokeRobot ();
+    }
+
+    public void revokeMine(Axis axis) {
+        this.plateau[ axis.getHeight () ][ axis.getWidth () ].revokeMine ();
     }
 
     private void initializeObstacles(double percentageObstacles) {
@@ -79,5 +100,79 @@ public class Plateau {
         }
 
         return tab;
+    }
+
+    @Override public String toString() {
+        StringBuilder results = new StringBuilder ("+");
+        for ( int i = 0; i < this.width; i++ )
+            results.insert (results.length (), "---+");
+        for ( int j = 0; j < this.length; j++ ) {
+            results.insert (results.length (), "\n| ");
+            for ( int i = 0; i < this.width; i++ ) {
+                if ( this.plateau[ i ][ j ] instanceof Base ) {
+                    switch ( this.plateau[ i ][ j ].getBase () ) {
+                        case 1:
+                            results.insert (results.length (), "B");
+                            break;
+                        case 2:
+                            results.insert (results.length (), "b");
+                            break;
+                        default:
+                            results.insert (results.length (), "");
+                            break;
+                    }
+                } else if ( this.plateau[ i ][ j ].getContent () instanceof Shooter ) {
+                    switch ( this.plateau[ i ][ j ].getContent ().getTeam () ) {
+                        case 1:
+                            results.insert (results.length (), "T");
+                            break;
+                        case 2:
+                            results.insert (results.length (), "t");
+                            break;
+                        default:
+                            results.insert (results.length (), "");
+                            break;
+                    }
+                } else if ( this.plateau[ i ][ j ].getContent () instanceof Char ) {
+                    switch ( this.plateau[ i ][ j ].getContent ().getTeam () ) {
+                        case 1:
+                            results.insert (results.length (), "C");
+                            break;
+                        case 2:
+                            results.insert (results.length (), "c");
+                            break;
+                        default:
+                            results.insert (results.length (), "");
+                            break;
+                    }
+                } else if ( this.plateau[ i ][ j ].getContent () instanceof Piegeur ) {
+                    switch ( this.plateau[ i ][ j ].getContent ().getTeam () ) {
+                        case 1:
+                            results.insert (results.length (), "P");
+                            break;
+                        case 2:
+                            results.insert (results.length (), "p");
+                            break;
+                        default:
+                            results.insert (results.length (), "");
+                            break;
+                    }
+                } else if ( this.plateau[ i ][ j ].getMine () == 1 ) {
+                    results.insert (results.length (), "X");
+                } else if ( this.plateau[ i ][ j ].getMine () == 2 ) {
+                    results.insert (results.length (), "x");
+                } else if ( this.plateau[ i ][ j ].getObstacle () ) {
+                    results.insert (results.length (), "O");
+                } else {
+                    results.insert (results.length (), " ");
+                }
+                results.insert (results.length (), " | ");
+            }
+            results.insert (results.length (), "\n+");
+            for ( int i = 0; i < this.largeur; i++ )
+                results.insert (results.length (), "---+");
+        }
+        results.insert (results.length (), "\n");
+        return results.toString ();
     }
 }
