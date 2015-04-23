827e5db7880009c0f3c4e22ce00abd64f26c5ab0
Boin Clément <boin.clement@outlook.com>
2015-04-23 20:33:14 +0200

    add team in Constants

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -12,5 +12,6 @@ public class Constants {
     public static final Random random = new Random (0);
     public static Scanner sc = new Scanner (System.in);
 
-
+    public static final int FIRST_TEAM = 1;
+    public static final int SECOND_TEAM = 2;
 }
