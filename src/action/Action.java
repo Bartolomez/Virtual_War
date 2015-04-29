9bb465b319b56f115fb82e68af7c5264f27aa5d3
Boin Clément <boin.clement@outlook.com>
2015-04-29 21:03:53 +0200

    A

---
+++ src/action/Action.java
@@ -0,0 +1,40 @@
+package action;
+
+import plateau.Axis;
+import robot.Robot;
+
+/**
+ * @author boinc
+ */
+
+public abstract class Action {
+    private Axis axis;
+    private Axis objective;
+    private Robot robot;
+
+    public Action() { }
+
+    public Action(Robot robot, Axis objective) {
+        this.robot = robot;
+        this.objective = objective;
+        this.axis = robot.getAxis ();
+    }
+
+    public Action(Robot robot) {
+        this(robot, null);
+    }
+
+    public Robot getRobot() {
+        return this.robot;
+    }
+
+    public Axis getPosition() {
+        return this.axis;
+    }
+
+    public Axis getObjective() {
+        return this.objective;
+    }
+
+    public abstract void doSomething();
+}
---
+++ src/action/Attack.java
@@ -0,0 +1,19 @@
+package action;
+
+import robot.Robot;
+
+/**
+ * @author boinc
+ */
+
+public class Attack extends Action {
+
+    public Attack(Robot robot) {
+        super(robot);
+    }
+
+    @Override public void doSomething() {
+
+    }
+
+}
