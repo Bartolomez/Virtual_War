c64c62fa2d78730484156aaff1b3b839c317c2d4
Boin Clément <boin.clement@outlook.com>
2015-05-07 14:07:56 +0200

    add some certif method

--- src/Main.java
+++ src/Main.java
@@ -16,13 +16,24 @@ public class Main {
         System.out.print("Largeur du plateau : ");
         int x = Integer.parseInt(Constants.sc.nextLine());
         System.out.print("Longeur du plateau : ");
-        int y = Integer.parseInt(Constants.sc.nextLine());
-        System.out.print("Pourcentage d'obstacle (de 0.0 a 1): ");
-        double obstacle = Double.parseDouble(Constants.sc.nextLine());
+        int y = Integer.parseInt( Constants.sc.nextLine() );
+        double obstacle;
+        do {
+            System.out.print("Pourcentage d'obstacle (de 0.1 à 0.99): ");
+            obstacle = Double.parseDouble(Constants.sc.nextLine());
+        } while(!isValide( obstacle ));
         Plateau plateau = new Plateau(x, y, obstacle);
         Team[] teams = new Team[2];
         teams[0] = new Team(new Axis(0, 0), plateau, 1);
-        teams[1] = new Team(new Axis(plateau.getLength() - 1, plateau.getWidth() - 1), plateau, 2);
+        int count = 0;
+        do {
+            if( count > 0 ) {
+                System.err.println( "Vous ne pouvez pas faire de coup d'état !!" );
+            }
+            teams[ 1 ] =
+                new Team( new Axis( plateau.getLength() - 1, plateau.getWidth() - 1 ), plateau, 2 );
+            count += 1;
+        } while (sameCountry(teams[0].getNomPays(), teams[1].getNomPays()));
         for (Team t : teams) {
             t.addRobot(new Tank(t.getView(), t));
             t.addRobot(new Scavenger(t.getView(), t));
@@ -32,7 +43,7 @@ public class Main {
         Robot robot;
         Action action;
         ArrayList<Robot> deadRobot = new ArrayList<Robot>();
-        int count = 0;
+        count = 0;
         do {
             System.out.println(teams[count % 2].getView().getPlateau() + "\n" + teams[count % 2]
                     .getNomPays() + " c'est à votre tour ! ");
@@ -66,6 +77,14 @@ public class Main {
                 deadRobot.clear();
             }
         } while (!end);
+    }
+
+    private static boolean sameCountry( String nomPays, String nomPays1 ) {
+        return nomPays.equals( nomPays1 );
+    }
+
+    private static boolean isValide( double obstacle ) {
+        return obstacle >= 0.1 && obstacle <= 0.99;
     }
 
 }
