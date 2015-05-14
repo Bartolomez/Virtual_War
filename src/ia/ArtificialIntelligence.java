ce99bb38e1c5bb4225840c2be0a48f427be06299
Boin Clément <boin.clement@outlook.com>
2015-05-14 19:32:29 +0200

    Implementation off all method for this class

--- src/ia/ArtificialIntelligence.java
+++ src/ia/ArtificialIntelligence.java
@@ -1,7 +1,99 @@
 package ia;
 
+import config.Constants;
+import plateau.Axis;
+import plateau.Plateau;
+import robot.Robot;
+import robot.Scavenger;
+import robot.Shooter;
+import robot.Tank;
+import team.Team;
+import team.View;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author boinc.
  */
+
 public class ArtificialIntelligence {
+    private String nomPays;
+    private Axis axisBase;
+    private View view;
+    private int team;
+    private List<Robot> robots;
+
+    public ArtificialIntelligence( Axis axisBase, Plateau plateau, int team ) {
+        this.nomPays = choseNomPays();
+        this.axisBase = axisBase;
+        this.view = new View( plateau, team );
+        this.team = team;
+        this.robots = choseRobots();
+    }
+
+    public ArtificialIntelligence( Axis axisBase, Plateau plateau, int team, String nomPays ) {
+        this.nomPays = choseNomPays( nomPays );
+        this.axisBase = axisBase;
+        this.view = new View( plateau, team );
+        this.team = team;
+        this.robots = choseRobots();
+    }
+
+    private List<Robot> choseRobots() {
+        List<Robot> robots = new ArrayList<>();
+        while( robots.size() != 5 ) {
+            switch( Constants.random.nextInt( 3 ) ) {
+                case 0:
+                    robots.add( new Tank( this.view,
+                        new Team( this.axisBase, this.view.getPlateau(), this.team ) ) );
+                    break;
+                case 1:
+                    robots.add( new Scavenger( this.view,
+                        new Team( this.axisBase, this.view.getPlateau(), this.team ) ) );
+                    break;
+                case 2:
+                    robots.add( new Shooter( this.view,
+                        new Team( this.axisBase, this.view.getPlateau(), this.team ) ) );
+                    break;
+            }
+        }
+        return robots;
+    }
+
+    private String choseNomPays() {
+        return Constants.NAME_COUNTRY[ Constants.random
+            .nextInt( Constants.NAME_COUNTRY.length - 1 ) ];
+    }
+
+    private String choseNomPays( String nomPays ) {
+        List<String> pays = new ArrayList<>( Arrays.asList( Constants.NAME_COUNTRY ) );
+        pays.remove( nomPays );
+        return pays.get( Constants.random.nextInt( pays.size() - 1 ) );
+    }
+
+    public Robot chooseRobot() {
+        return this.robots.get( Constants.random.nextInt(robots.size() - 1) );
+    }
+
+    public int chooseAction(int borderA, int borderB) {
+        return Constants.random.nextInt( borderB - borderA );
+    }
+
+    public List<Robot> getRobots() {
+        return robots;
+    }
+
+    public String getNomPays() {
+        return nomPays;
+    }
+
+    public boolean lose() {
+        return robots.isEmpty();
+    }
+
+    public View getView() {
+        return this.view;
+    }
 }
