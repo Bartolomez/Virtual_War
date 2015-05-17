9b8ecc83af238c7d989b63015e3c66fb60777f3e
Pitidej <pumas1600@hotmail.fr>
2015-05-17 15:58:04 +0200

    Javadoc Package ia
    /!\ Dans la fonction chooseAction, les @param ne sont pas indiqués.

--- src/ia/ArtificialIntelligence.java
+++ src/ia/ArtificialIntelligence.java
@@ -15,20 +15,36 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * La classe ArtificialIntelligence permet de creer une IA et de gerer celle ci.
+ * 
  * @author seysn
  * @author boinc.
  */
 
 public class ArtificialIntelligence extends Team {
 
+	/** Le nombre de robot gerer par l IA */
     public static int nbRobots;
 
+    /**
+     * Construit un objet ArtificialIntelligence avec la coordonnee de la base, le plateau et le numero de l equipe passee en parametre.
+     * @param axisBase - La coordonnee de la base de l equipe geree par l IA.
+     * @param plateau - Le plateau sur lequel joue l IA.
+     * @param team - Le numero de l equipe geree par l IA.
+     */
     public ArtificialIntelligence(Axis axisBase, Plateau plateau, int team) {
         super(axisBase, plateau, team);
         robots = chooseRobots();
         setIsIa(true);
     }
 
+    /**
+     * Construit un objet ArtificialIntelligence avec la coordonnee de la base, le plateau, le numero et le nom de pays de l equipe passee en parametre.
+     * @param axisBase - La coordonnee de la base de l equipe geree par l IA.
+     * @param plateau - Le plateau sur lequel joue l IA.
+     * @param team - Le numero de l equipe geree par l IA.
+     * @param nomPays - Le pays de l equipe geree par l IA.
+     */
     public ArtificialIntelligence(Axis axisBase, Plateau plateau, int team, String nomPays) {
         super(axisBase, plateau, team, nomPays);
         robots = chooseRobots();
@@ -73,22 +89,44 @@ public class ArtificialIntelligence extends Team {
         return pays.get(Constants.random.nextInt(pays.size() - 1));
     }
 
+    /**
+     * Choisis un Robot pour l IA.
+     * @return Une instance de Robot, qui correspond au Robot choisis aleatoirement pour l IA.
+     */
     public Robot chooseRobot() {
         return this.robots.get(Constants.random.nextInt(robots.size() - 1));
     }
 
+    /**
+     * Choisis une action pour l IA.
+     * @param borderA - Un entier.
+     * @param borderB - Un deuxieme entier.
+     * @return Une entier qui correspond a une action choisie aleatoirement pour l IA.
+     */
     public int chooseAction(int borderA, int borderB) {
         return Constants.random.nextInt(borderB - borderA);
     }
 
+    /**
+     * Retourne la liste des Robots de l equipe geree par l IA.
+     * @return Une liste de Robot, qui correspond a la liste des robots de l equipe geree par l IA.
+     */
     public List<Robot> getRobots() {
         return robots;
     }
 
+    /**
+     * Indique si l IA a perdu.
+     * @return Un booleen, qui est vrai si l IA a perdu, faux dans le cas contraire.
+     */
     public boolean lose() {
         return robots.isEmpty();
     }
 
+    /**
+     * Retourne une chaine de caractere pour l objet ArtificialIntelligence.
+     * @return Une chaine de caractere, pour l objet ArtificialIntelligence.
+     */
     public String toString() {
         return "IA " + super.toString();
     }
