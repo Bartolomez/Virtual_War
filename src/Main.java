a0a0e7b6aef4511b299d2b188f32938aea77739b
Nicolas Seys <seys.nicolas@gmail.com>
2015-05-20 10:20:37 +0200

    nothing

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
  * @author seysn
  */
 public class Main {
-
+ 
     public static void main( String[] args ) throws Exception {
         Robot robot;
         Action action;
