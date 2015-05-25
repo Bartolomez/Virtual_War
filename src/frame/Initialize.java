7d988798abbb67cfc127e9369daafca5cf7bf06f
Nicolas Seys <seys.nicolas@gmail.com>
2015-05-25 18:43:06 +0200

    changed the place of declarations

--- src/frame/Initialize.java
+++ src/frame/Initialize.java
@@ -6,16 +6,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 
 /**
  * La fenetre permettant d'initialiser le plateau et les équipes
- * TODO : rendre la frame fonctionnelle
  *
  * @author seysn
  */
 public class Initialize extends JFrame {
+    public JTextField larg, haut, pourc, nbTank1, nbTank2, nbShooter1, nbShooter2, nbScavenger1,
+            nbScavenger2;
+    public JComboBox type1, type2, pays1, pays2;
+    public JButton valider, retour;
 
     public Initialize() {
         init();
@@ -25,68 +27,68 @@ public class Initialize extends JFrame {
         setVisible(true);
     }
 
-    private void init(){
+    private void init() {
         setLayout(new BorderLayout());
         JPanel pageStart = new JPanel(new GridLayout(0, 2));
         pageStart.add(new JLabel("Largeur"));
-        JTextField larg = new JTextField("10");
+        larg = new JTextField("10");
         pageStart.add(larg);
         pageStart.add(new JLabel("Hauteur"));
-        JTextField haut = new JTextField("10");
+        haut = new JTextField("10");
         pageStart.add(haut);
         pageStart.add(new JLabel("% d'obstacles"));
-        JTextField pourc = new JTextField("25");
+        pourc = new JTextField("25");
         pageStart.add(pourc);
         pageStart.setBorder(BorderFactory
                 .createTitledBorder(BorderFactory.createLineBorder(new Color(0)), "Plateau"));
         add(pageStart, BorderLayout.PAGE_START);
 
         JPanel team1 = new JPanel(new GridLayout(0, 2));
-        JComboBox type = new JComboBox(new String[]{"Ordinateur","Joueur"});
+        type1 = new JComboBox(new String[] {"Ordinateur", "Joueur"});
         team1.add(new JLabel("Type"));
-        team1.add(type);
-        JComboBox pays1 = new JComboBox(Constants.NAME_COUNTRY);
+        team1.add(type1);
+        pays1 = new JComboBox(Constants.NAME_COUNTRY);
         team1.add(new JLabel("Pays"));
         team1.add(pays1);
         team1.setBorder(
                 BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(), "Equipe 1"));
-        JTextField nbTank1 = new JTextField("1");
+        nbTank1 = new JTextField("1");
         team1.add(new JLabel("Char"));
         team1.add(nbTank1);
-        JTextField nbShooter1 = new JTextField("1");
+        nbShooter1 = new JTextField("1");
         team1.add(new JLabel("Tireur"));
         team1.add(nbShooter1);
-        JTextField nbScavenger1 = new JTextField("1");
+        nbScavenger1 = new JTextField("1");
         team1.add(new JLabel("Piegeur"));
         team1.add(nbScavenger1);
 
         add(team1, BorderLayout.LINE_START);
 
         JPanel team2 = new JPanel(new GridLayout(0, 2));
-        JComboBox type2 = new JComboBox(new String[]{"Ordinateur","Joueur"});
+        type2 = new JComboBox(new String[] {"Ordinateur", "Joueur"});
         team2.add(new JLabel("Type"));
         team2.add(type2);
-        JComboBox pays2 = new JComboBox(Constants.NAME_COUNTRY);
+        pays2 = new JComboBox(Constants.NAME_COUNTRY);
         team2.add(new JLabel("Pays"));
         team2.add(pays2);
         team2.setBorder(
                 BorderFactory.createTitledBorder(BorderFactory.createEmptyBorder(), "Equipe 2"));
-        JTextField nbTank2 = new JTextField("1");
+        nbTank2 = new JTextField("1");
         team2.add(new JLabel("Char"));
         team2.add(nbTank2);
-        JTextField nbShooter2 = new JTextField("1");
+        nbShooter2 = new JTextField("1");
         team2.add(new JLabel("Tireur"));
         team2.add(nbShooter2);
-        JTextField nbScavenger2 = new JTextField("1");
+        nbScavenger2 = new JTextField("1");
         team2.add(new JLabel("Piegeur"));
         team2.add(nbScavenger2);
 
         add(team2, BorderLayout.LINE_END);
 
         JPanel panelValider = new JPanel();
-        JButton valider = new JButton("Valider");
+        valider = new JButton("Valider");
         panelValider.add(valider);
-        JButton retour = new JButton("Retour");
+        retour = new JButton("Retour");
         retour.addMouseListener(new MouseAdapter() {
             @Override public void mouseClicked(MouseEvent event) {
                 System.exit(0);
