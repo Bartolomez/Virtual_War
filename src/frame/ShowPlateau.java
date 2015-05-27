48103198d820542309c7057b97a9d892895d25f9
Nicolas Seys <seys.nicolas@gmail.com>
2015-05-27 12:47:30 +0200

    updated methods

--- src/frame/ShowPlateau.java
+++ src/frame/ShowPlateau.java
@@ -1,7 +1,10 @@
 package frame;
 
+import config.Constants;
+import plateau.Axis;
 import plateau.Plateau;
 import robot.Robot;
+import team.Team;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,39 +16,133 @@ import java.util.List;
 /**
  * @author seysn
  */
-public class ShowPlateau extends JPanel {
-
-    private List<MyCell> list       = new ArrayList<>();
-    private int          tailleCase = 30;
-    private Plateau p;
-    private Robot   selected;
+public class ShowPlateau extends JFrame {
+    public         JLabel       title;
+    public static  JPanel       footer;
+    public         PanelPlateau pane;
+    public static  Robot        selected;
+    private static Team         teamCourante;
+    public static ArrayList<JButton> buttons = new ArrayList<>();
+    private static JPanel paneButtons;
 
     public ShowPlateau(Plateau p) {
-        this.p = p;
+        setTitle("Jeu");
+        setResizable(false);
+        pane = new PanelPlateau(p);
         init();
-        this.setPreferredSize(new Dimension(tailleCase * p.getLength(), tailleCase * p.getWidth()));
-        this.addMouseListener(new MouseAdapter() {
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void init() {
+        title = new JLabel();
+        title.setHorizontalAlignment(JLabel.CENTER);
+        title.setPreferredSize(new Dimension(30, 30));
+        setLayout(new BorderLayout());
+        add(title, BorderLayout.NORTH);
+        add(pane, BorderLayout.CENTER);
+        footer = new JPanel();
+        JLabel info = new JLabel();
+        paneButtons = new JPanel();
+        footer.add(info);
+        footer.add(paneButtons);
+        footer.setPreferredSize(new Dimension(100, 100));
+        pane.addMouseListener(new MouseAdapter() {
             @Override public void mouseClicked(MouseEvent event) {
-                for (MyCell c : list) {
-                    if (c.contains(event.getX(), event.getY()) && c.isRobotTeam1() || c
-                            .isRobotTeam2()) {
-                        selected =
-                                p.getCell((int) c.getX() / tailleCase, (int) c.getY() / tailleCase)
-                                        .getRobot();
-                    } else {
+                for (MyCell c : pane.list) {
+                    if (c.contains(event.getX(), event.getY()) && (c.isRobotTeam1() || c
+                            .isRobotTeam2())) {
+                        info.setText("Vous avez selectionné un Robot");
+                        selected = pane.p.getCell((int) c.getX() / pane.tailleCase,
+                                (int) c.getY() / pane.tailleCase).getRobot();
+                        addButtons(selected);
+                        repaint();
+                    } else if (c.contains(event.getX(), event.getY()) && c.isBaseTeam1()) {
                         selected = null;
+                        info.setText("Vous avez selectionné votre Base");
+                        addButtons();
+                        repaint();
+                    } else if (c.contains(event.getX(), event.getY()) && c.isBaseTeam2()) {
+                        selected = null;
+                        info.setText("Vous avez selectionné la Base adverse");
+                        repaint();
+                    } else if (c.contains(event.getX(), event.getY()) && (c.isVide() || c
+                            .isObstacle())) {
+                        selected = null;
+                        info.setText("Vous n'avez rien selectionné");
+                        repaint();
                     }
                 }
             }
         });
+        add(footer, BorderLayout.SOUTH);
+    }
+
+    public void changeTitle(int tour, String name) {
+        this.title.setText("[Tour " + tour + "] " + name);
+    }
+
+    public static void addButtons() {
+        for (JButton b : buttons) {
+            buttons.remove(b);
+        }
+        for (Robot r : teamCourante.getRobots()) {
+            if (r.getAxis().equals(teamCourante.getAxisBase())) {
+                buttons.add(new JButton(r.getType() + " Energie : " + r.getEnergy()));
+                paneButtons.add(buttons.get(buttons.size() - 1));
+                buttons.get(buttons.size() - 1).addMouseListener(new MouseAdapter() {
+                    @Override public void mouseClicked(MouseEvent event) {
+                        selected = r;
+                        addButtons(r);
+                    }
+                });
+            }
+        }
+    }
+
+    public static void addButtons(Robot r) {
+        for (JButton b : buttons) {
+            buttons.remove(b);
+        }
+        for (Axis a : r.searchMoves()) {
+            buttons.add(new JButton("Deplacement vers " + a));
+        }
+    }
+
+    public static void setTeamCourante(Team teamCourante) {
+        ShowPlateau.teamCourante = teamCourante;
+    }
+
+    public static void main(String[] args) {
+        ShowPlateau s = new ShowPlateau(new Plateau(10, 10, 0.2));
+        s.changeTitle(3, "France");
+    }
+}
+
+
+class PanelPlateau extends JPanel {
+
+    public List<MyCell> list       = new ArrayList<>();
+    public int          tailleCase = 30;
+    public Plateau p;
+
+    public PanelPlateau(Plateau p) {
+        this.p = p;
+        init();
+        this.setPreferredSize(new Dimension(tailleCase * p.getLength(), tailleCase * p.getWidth()));
     }
 
     private void init() {
         for (int i = 0; i < p.getLength(); i++) {
             for (int j = 0; j < p.getWidth(); j++) {
-                if (p.getCell(i, j).getBase() != 0) {
+                if (p.getCell(i, j).getBase() == Constants.FIRST_TEAM) {
                     list.add(new MyCell(tailleCase * i, tailleCase * j, tailleCase, tailleCase,
-                            "Base"));
+                            "Base1"));
+                } else if (p.getCell(i, j).getBase() == Constants.SECOND_TEAM) {
+                    list.add(new MyCell(tailleCase * i, tailleCase * j, tailleCase, tailleCase,
+                            "Base2"));
                 } else if (p.getCell(i, j).isObstacle()) {
                     list.add(new MyCell(tailleCase * i, tailleCase * j, tailleCase, tailleCase,
                             "Obstacle"));
@@ -69,7 +166,7 @@ public class ShowPlateau extends JPanel {
         for (MyCell c : list) {
             if (c.isObstacle()) {
                 graphics.setColor(new Color(0));
-            } else if (c.isBase()) {
+            } else if (c.isBaseTeam1() || c.isBaseTeam2()) {
                 graphics.setColor(new Color(0x0EFF00));
             } else if (c.isVide()) {
                 graphics.setColor(new Color(0xFFFFFF));
@@ -83,9 +180,6 @@ public class ShowPlateau extends JPanel {
         }
     }
 
-    public Robot getSelectedRobot() {
-        return selected;
-    }
 }
 
 
@@ -99,7 +193,9 @@ class MyCell extends Rectangle {
 
     public boolean isVide() { return type.equals("Vide"); }
 
-    public boolean isBase() { return type.equals("Base"); }
+    public boolean isBaseTeam1() { return type.equals("Base1"); }
+
+    public boolean isBaseTeam2() { return type.equals("Base2"); }
 
     public boolean isObstacle() { return type.equals("Obstacle"); }
 
