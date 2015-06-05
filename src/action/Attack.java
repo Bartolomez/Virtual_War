110fb68ed1e6758dc5985288a3cb541b9c603409
Boin Clément <clem.boin@gmail.com>
2015-06-05 21:07:37 +0200

    Remove execption

--- src/action/Attack.java
+++ src/action/Attack.java
@@ -11,30 +11,34 @@ import robot.Robot;
 
 public class Attack extends Action {
 
-    /**
-     * Construit un objet Attack avec le robot passe en parametre.
-     *
-     * @param robot - Le robot qui realise l attaque.
-     */
-    public Attack( Robot robot ) {
-        super( robot );
-    }
+  /**
+   * Construit un objet Attack avec le robot passe en parametre.
+   *
+   * @param robot - Le robot qui realise l attaque.
+   */
+  public Attack(Robot robot) {
+    super(robot);
+  }
 
-    public Attack( Robot robot, Axis objective) {
-        super( robot, objective );
-    }
+  public Attack(Robot robot, Axis objective) {
+    super(robot, objective);
+  }
 
-    @Override public void doSomething() {
-        System.out.println(
-            "Vous allez prendre pour cible : " + this.getRobot().getView().getPlateau()
-                .getCell( getObjective() ).getRobot() + " avec une puissance de " + this.getRobot()
-                .getDamageByShoot() );
-        this.getRobot().getView().getPlateau().getCell( getObjective() ).getRobot()
-            .suddenByShoot( this.getRobot() );
-        System.out.println(
-            "Après votre attaque la cible est tombé à " + this.getRobot().getView().getPlateau()
-                .getCell( getObjective() ).getRobot().getEnergy() + " pv" );
-        this.getRobot().losesEnergyAfterAction();
+  @Override
+  public void doSomething() {
+    try {
+      System.out.println("Vous allez prendre pour cible : "
+          + this.getRobot().getView().getPlateau().getCell(getObjective()).getRobot()
+          + " avec une puissance de " + this.getRobot().getDamageByShoot());
+      this.getRobot().getView().getPlateau().getCell(getObjective()).getRobot()
+          .suddenByShoot(this.getRobot());
+      System.out.println("Après votre attaque la cible est tombé à "
+          + this.getRobot().getView().getPlateau().getCell(getObjective()).getRobot().getEnergy()
+          + " pv");
+      this.getRobot().losesEnergyAfterAction();
+    } catch (Exception e) {
+
     }
+  }
 
 }
